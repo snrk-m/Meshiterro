6eb6e2b3c15ec22014f28313a3488effc0554c0a
snrk-m <mrt.bearpu3@gmail.com>
2021-03-18 06:31:50 +0000

    finish meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'fa01f493fb46add45f9aa1ed15ce8a5f0ccce3a7ebc54b13f5b8ae956861a081f186ef5cbead122f54b0706588d18a47c14c977554d02e50d1abc3da65ac7852'
